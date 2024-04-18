a24221cb1777537d77d0857cab9b79f817848cd3
Liand <72455406@continental.du.pe>
2024-04-18 18:33:52 -0500

    Correccion al ejemplo 2

---
+++ Ejemploo1.py
@@ -0,0 +1,10 @@
+numero1 = 10
+numero2 = 5
+suma = numero1 + numero2
+
+def funcion_multiplicar(numero1, numero2):
+    multiplicacion = numero1 * numero2
+    return multiplicacion
+
+resultado = funcion_multiplicar(numero1, numero2)
+print(resultado)
---
+++ Ejemploo3.py
@@ -0,0 +1,22 @@
+def area_Rec(ancho, largo):
+    result = ancho * largo
+    return result
+
+# Función para calcular el área de un triángulo
+def area_Tri(b, h):
+    area = (b * h)/2
+    return r
+
+# Función principal
+def principal():
+    x = 4
+    y = 6
+    rect_area = f(x, y)
+    print("Área del rectángulo:", area_Rec)
+
+    base = 5
+    altura = 8
+    tri_area = g(base, altura)
+    print("Área del triángulo:", area_Tri)
+
+main()
